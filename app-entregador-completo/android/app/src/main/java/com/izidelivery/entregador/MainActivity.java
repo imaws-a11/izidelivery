fae5a812220baef014889dd38f77ed455db69291
swmcapital <swmcapital@gmail.com>
2026-04-24 16:58:28 -0300

    feat: implement Izi Black VIP management module and add notification sound assets

--- app-entregador-completo/android/app/src/main/java/com/izidelivery/entregador/MainActivity.java
+++ app-entregador-completo/android/app/src/main/java/com/izidelivery/entregador/MainActivity.java
@@ -1,5 +1,23 @@
 package com.izidelivery.entregador;
 
+import android.os.Bundle;
+import android.os.Build;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Permite que o app "acorde" a tela e apareça sobre o lockscreen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+        } else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+}
